3e3ac6bad5165ea1cf29c1947c2a6a38e302c60d
lmalleret <lmalleret.99@gmail.com>
2024-07-17 16:43:31 -0300

    Subiendo implementación lógica drag & drop de las tareas

--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -1,3 +1,4 @@
+import { Droppable } from "react-beautiful-dnd";
 import { mapOrder } from "../../utilities/sorts";
 import Task from "../task/Task";
 import "./Column.scss";
@@ -7,17 +8,24 @@ function Column(props: any) {
   const { column } = props;
   const tasks = mapOrder(column.tasks, column.taskOrder, "id");
   return (
-    <>
-      <div className="column">
-        <header>{column.title}</header>
-        <ul className="task-list">
-          {tasks &&
-            !_.isEmpty(tasks) &&
-            tasks.map((task: any) => <Task key={task.id} task={task} />)}
-        </ul>
-        <footer>Add another card</footer>
-      </div>
-    </>
+    <div className="column">
+      <header>{column.title}</header>
+      <Droppable droppableId={column.id}>
+        {(provided) => (
+          <ul
+            className="task-list"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {tasks.map((task: any, index: number) => (
+              <Task key={task.id} task={task} index={index} />
+            ))}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+      <footer>Add another card</footer>
+    </div>
   );
 }
 
--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,17 +1,24 @@
+import { Draggable } from "react-beautiful-dnd";
 import "./Task.scss";
 
 function Task(props: any) {
-  const { task } = props;
-  console.log(task);
+  const { task, index } = props;
   return (
-    <>
-      <li className="task-item">
-        {task.image && (
-          <img className="card-cover" src={task.image} alt="card image" />
-        )}
-        {task.title}
-      </li>
-    </>
+    <Draggable draggableId={task.id} index={index}>
+      {(provided) => (
+        <li
+          className="task-item"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          {task.image && (
+            <img className="card-cover" src={task.image} alt="card image" />
+          )}
+          {task.title}
+        </li>
+      )}
+    </Draggable>
   );
 }
 
