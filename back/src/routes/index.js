f11deb76cacff9c482ef6a346d0c7f9eb29527f8
lmalleret <lmalleret.99@gmail.com>
2024-07-21 15:40:15 -0300

    Se quito el backend para la aplicaicón de trello

--- back/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const boardController = require('../controllers/boardController');
-
-router.get('/boards', boardController.getAllBoards);
-
-module.exports = router;
